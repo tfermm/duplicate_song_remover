e2881b55f755df454b58c8517bbfe7f27d7e8418
tfermm <tfermm@gmail.com>
2013-04-11 01:37:59 -0400

    Script now stores all the data in a dictionary and prints all songs out based off of artist

--- main.py
+++ main.py
@@ -2,31 +2,47 @@ import unicodedata
 import mutagen
 from mutagen.mp4 import MP4
 import os
-
+ 
 import glob
-
+ 
 directory = "."
-
+ 
 f = []
 f = glob.glob(directory + "/*.m4a")
-
+music = {}
 for i in f:
  	split = i.split(".")
-	split = split[2]
+ 	split = split[2]
+   
+ 	if split == ("m4a"):
+ 		audio = MP4(i) 
+ 
+ 		x = audio.pprint()
+ 		bitrate = audio.info.bitrate
+ 		audioDecoded = x.encode('ascii', 'ignore').split('\n')
 
-	if split == ("m4a"):
-		audio = MP4(i) 
-
-		x = audio.pprint()
-		audio = x.encode('ascii', 'ignore').split('\n')
-		for j in audio:
-#			print j
-			temp = j.split("=")
-			tag = temp[0]
-			if tag == "nam":
-				temp[0] = "";
-				name = " ".join(temp).lstrip()
-			elif tag == "ART":
-				temp[0] = "";
-				artist = " ".join(temp).lstrip()
-				print artist
+		artist=""
+		name=""
+ 		for j in audioDecoded:
+ 			temp = j.split("=")
+ 			tag = temp[0]
+ 			if tag == "nam":
+ 				temp[0] = "";
+ 				name = " ".join(temp).lstrip()
+ 			elif tag == "ART":
+ 				temp[0] = "";
+ 				artist = " ".join(temp).lstrip()
+			if not music.has_key(artist):
+				music[artist] = {}
+			music[artist][name] = {}
+			music[artist][name]["name"] = name;
+			music[artist][name]["bitrate"] = bitrate;
+			music[artist][artist] = artist
+for i in range(1, len(music)):
+	artist = music.keys()[i]
+	print music.keys()[i] + "\n"
+	songs = music.get(artist)
+	for i in range(2, len(songs)):
+		song = songs.keys()[i]
+		print "\t" + song
+	print "\n"
